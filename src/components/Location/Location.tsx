159105ab215cd78e0132f504460ba8236c81196e
Anton Babchenkov <anton.babchekov@olo.com>
2022-03-21 12:24:25 +0100

    styles changed

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -4,8 +4,11 @@ import Loader from "../Loader/Loader";
 import { useTranslation } from "react-i18next";
 function Location() {
   const { t } = useTranslation();
+
   const [loading, setLoading] = useState(true);
+
   const loader = loading ? <Loader /> : null;
+
   return (
     <div className={"location-container"}>
       <h3>{t("location")}</h3>
